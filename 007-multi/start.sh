2c576950101a0493d46e54823e9a01f598c39f6a
amarchino <marchino.alessandro@gmail.com>
2020-12-01 17:44:22 +0100

    Lezione 86

--- 007-multi/start.sh
+++ 007-multi/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --name 007-mongo    --rm -d --network 007-multi mongo
-docker run --name 007-backend  --rm -d --network 007-multi -p 80:80 007-multi:backend
-docker run --name 007-frontend --rm -d --network 007-multi -p 3000:3000 -it 007-multi:frontend
+docker run --name 007-mongo --rm -e MONGO_INITDB_ROOT_USERNAME=max -e MONGO_INITDB_ROOT_PASSWORD=secret --network 007-net-multi -v 007-data-mongo:/data/db -d mongo
+docker run --name 007-backend --rm -e MONGODB_USERNAME=max -d --network 007-net-multi -p 80:80 -v 007-data-logs:/app/logs -v $PWD/backend:/app 007-multi:backend
+docker run --name 007-frontend --rm -d --network 007-net-multi -p 3000:3000 -it -v $PWD/frontend:/app 007-multi:frontend
